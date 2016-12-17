3ab26c05e7dc5eafa1e3367afbfebf9672285f0b
paulxu21 <paul_xu21@hotmail.com>
2016-12-17 10:24:09 +0800

    Generate
    Generate by paul

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String str = "aaabbb";
+        String str1 = str;
+        System.out.println("str1:" + str1);
+        str = "cccddd";
+        System.out.println("str: " + str1);
+
+     }
+}
